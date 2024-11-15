43b961e956d3dd593b00f6641249d77ba11e6e35
Shubham Gavkare <109575865+GavkareShubham@users.noreply.github.com>
2024-11-15 17:29:03 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
+const uri = "mongodb+srv://shubhamgavkare07:vSHFNP4U40mEh8h3@cluster0.mongodb.net/mernstack?retryWrites=true&w=majority";
 
-mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
+mongoose.connect(uri)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB Atlas", err);
+  });
 
 const userSchema = mongoose.Schema({
     image: String,
     email: String,
     name: String
-})
+});
 
 module.exports = mongoose.model('user', userSchema);
